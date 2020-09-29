6d08f493dae745a553f5dc6a420e26acb27e46a0
viktorprogger <viktorprogger@gmail.com>
2020-09-29 16:29:54 +0300

    Move action config example to the example file

--- config/actions.php
+++ config/actions.php
@@ -2,33 +2,4 @@
 
 declare(strict_types=1);
 
-use Evento\Dispatcher\Handler\DeferredHandler;
-use Evento\Dispatcher\Handler\HandlerInterface;
-use Yiisoft\Validator\Rule\Url;
-use Yiisoft\Validator\Rules;
-
-return [
-    'action' => [
-        'action2',
-        [
-            'action' => 'action3',
-            'conditions' => new Rules(/* some rules here */),
-        ],
-        [
-            'action' => 'action4',
-            'conditions' => static fn () => new Rules(/* some rules here */),
-        ],
-        [
-            'action' => 'action5',
-            'conditions' => [
-                static fn ($payload, CustomDependency $dependency) => $dependency->suitesAction($payload),
-                (new Url())->enableIDN(),
-            ],
-        ],
-        [
-            'action' => 'action6',
-            '__class' => DeferredHandler::class,
-        ],
-        static fn (): HandlerInterface => new CustomHandler(),
-    ],
-];
+return [];
